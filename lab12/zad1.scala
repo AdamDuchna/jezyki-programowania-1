b57714be2894e0d8a5713741046dba05b5679c26
Adam Duchna <adamduchna43@gmail.com>
2021-05-21 13:38:50 +0200

    lab12 zrobione

--- lab12/zad1.scala
+++ lab12/zad1.scala
@@ -1,83 +1,155 @@
-import akka.actor.{ActorSystem, Actor, Props}
+import akka.actor.{ActorSystem, Actor, Props,ActorRef,Terminated,PoisonPill}
 import scala.concurrent.duration._
-/*
-  W konfiguracji projektu wykorzystana została wtyczka
-  sbt-revolver. W związku z tym uruchamiamy program poleceniem
+import scala.util.Random
 
-    reStart
-
-  a zatrzymujemy pisząc (mimo przesuwających się komunikatów)
-
-     reStop
-
-  i naciskając klawisz ENTER. Jeśli czynności powyższe
-  już wykonywaliśmy to możemy też przywołać poprzednie
-  polecenia używając strzałek góra/dół na klawiaturze.
-*/
-
-// Przykład wykorzystania Planisty (Scheduler)
 case object Start
+case class StartCastle(enemy: ActorRef,strat: Int)
 case class Ask(ref: ActorRef)
 case class SendCount(n: Int)
 case object Count
+case object Hit
 case object Kill
+case class Shoot(n: Int)
+case object End
 
-object Planista extends Actor {
+
+class Planista extends Actor {
   def receive: Receive={
-    case Start => 
+    case Start =>{
       val castle1 = context.actorOf(Props[Zamek],"zamek1")
       val castle2 = context.actorOf(Props[Zamek],"zamek2")
-      context.become(ready(castle1+castle2))
-      castle1 ! Start
-      castle2 ! Start
-  def ready(castles): Receive={
-    case CountArchers =>
-    castles[0] ! CountArchers(castles[1])
-    castles[1] ! CountArchers(castles[0])
-
-  }
-  }
-
-  object Zamek extends Actor {
-    def receive: Receive ={
-      case Start =>
-        val strzelcy=for( i <- 0 to 99 ) yield context.actorOf(Props[Lucznik],"lucznik"+i)
-        context.become(ready(strzelcy,100))
+      val sekw= IndexedSeq(castle1,castle2)
+      context.become(ready(sekw))
+      castle1 ! StartCastle(castle2,2)
+      castle2 ! StartCastle(castle1,3)
+      self ! Count
     }
-    def ready(strzelcy,wrogowie): Receive ={
-      case Ask(reference)=>
-        reference ! Count
-      case Count=>
-        sender ! SendCount(strzelcy.length)
-      case SendCount(n)=>
-        context.become(strzelcy,n)
-
+  }
       
+  def ready(castles: IndexedSeq[ActorRef]): Receive={
+    case Count =>{
+    castles(0) ! Shoot(1)
+    castles(1) ! Shoot(1)   
     }
-    object Lucznik extends Actor{
-      def receive: Receive={
-
-      }
-
-    }
-  }
-
-  }
-
-  def main(args: Array[String]): Unit = {
-    val system = ActorSystem("system")
-    import system.dispatcher
-
-    val tickActor = system.actorOf(Props[TickActor](), "defender")
-
-    val ticker = system.scheduler.scheduleWithFixedDelay(
-      Duration.Zero,
-      50.milliseconds,
-      tickActor,
-      TickActor.Tick
-    )
-
-    // system.terminate()
-
+    case End =>context.become(receive)
   }
 }
+
+
+class Zamek extends Actor {
+  def receive: Receive ={
+    case StartCastle(enemy,strat) =>{
+      val strzelcy=for( i <- 0 to 99 ) yield context.actorOf(Props[Lucznik],"lucznik"+i)
+      context.become(shoot(strzelcy,400,100,enemy,sender,strat))
+
+    }
+  }
+     
+  def shoot(strzelcy: IndexedSeq[ActorRef],rezerwa: Int,wrogowie: Int,wrogZamek: ActorRef,nadzorca: ActorRef,strat: Int): Receive ={
+
+    case Count=> sender ! SendCount(strzelcy.length)
+    case SendCount(n)=> context.become(shoot(strzelcy,rezerwa,n,sender,nadzorca,strat))
+    case Shoot(_)=>{
+      for (i <- 0 to strzelcy.length-1){
+      wrogZamek ! Count
+      strzelcy(i) ! Shoot(wrogowie)
+      }
+    }
+    case Hit=> wrogZamek ! Kill
+    case Kill=>{
+    if (strzelcy.length>0){
+      context.watch(strzelcy.last)
+      strzelcy.last ! PoisonPill
+      }
+    else{
+      println(wrogZamek.path.name+" wygrał")
+      context.become(receive)
+      nadzorca ! End
+    }
+    }
+    case Terminated(actor) =>{
+      println(self.path.name,strzelcy.length,rezerwa)
+      if((rezerwa <= 0)){
+        if(strzelcy.length == 0){ println(wrogZamek.path.name+" wygrał")
+          context.become(receive)
+          nadzorca ! End}
+        else{
+          val removed=strzelcy.drop(1)
+          context.become(shoot(removed,rezerwa,wrogowie,wrogZamek,nadzorca,strat))
+        }  
+      }
+      
+      else{
+        val removed=strzelcy.dropRight(1)
+        //STRATEGIA 1 LEPSZA NIZ WSZYSTKIE POZOSTAŁE
+        //Utrzymuj 100 jednostek
+        if(strat == 1){
+        val withNew=removed :+ context.actorOf(Props[Lucznik],"lucznikNew")
+        context.become(shoot(withNew,rezerwa-1,wrogowie,wrogZamek,nadzorca,strat))
+        }
+        //STRATEGIA 2 > 4, < 1, <3
+        //Gdy jest jeden strzelec wypełnij do 100
+        if(strat == 2){
+          if(strzelcy.length == 1){
+          if (rezerwa > 98){
+            val newArch = for(i <- 1 to 99) yield context.actorOf(Props[Lucznik],"lucznikNew"+i)
+            context.become(shoot(newArch ++ removed,rezerwa-99,wrogowie,wrogZamek,nadzorca,strat))
+            }
+          else{
+            val newArch = for(i <- 1 to rezerwa+1) yield context.actorOf(Props[Lucznik],"lucznikNew"+i)
+            context.become(shoot(newArch ++ removed,0,wrogowie,wrogZamek,nadzorca,strat))
+            }
+          }
+          else{context.become(shoot(removed,rezerwa-1,wrogowie,wrogZamek,nadzorca,strat))}
+        }
+        //STRATEGIA 3 < 4, < 1, >2
+        //Utrzymywanie 2 jednostek
+        if(strat == 3){
+          if(strzelcy.length == 1){
+          val withNew=removed :+ context.actorOf(Props[Lucznik],"lucznikNew")
+          context.become(shoot(withNew,rezerwa-1,wrogowie,wrogZamek,nadzorca,strat))
+          }
+          else{context.become(shoot(removed,rezerwa-1,wrogowie,wrogZamek,nadzorca,strat))}
+        }
+        //STRATEGIA 4 > 3, < 1, < 2
+        //Utrzymywanie 25 jednostek
+        if(strat==4){
+          if(strzelcy.length == 24){
+          val withNew=removed :+ context.actorOf(Props[Lucznik],"lucznikNew")
+          context.become(shoot(withNew,rezerwa-1,wrogowie,wrogZamek,nadzorca,strat))
+          }
+          else{context.become(shoot(removed,rezerwa-1,wrogowie,wrogZamek,nadzorca,strat))}
+        }
+      }
+    }
+  }
+}
+class Lucznik extends Actor{
+  def receive: Receive={
+    case Shoot(num) =>{ 
+      val prob=num/2
+      val getRand=Random.between(0,101)
+      if(getRand<=prob){
+        sender ! Hit}
+    }
+  }
+}
+
+object Main{
+  def main(args: Array[String]): Unit = {
+      val system = ActorSystem("system")
+      import system.dispatcher
+      val planner= system.actorOf(Props[Planista], "planista")
+
+      planner ! Start
+
+      val ticker = system.scheduler.scheduleWithFixedDelay(
+        Duration.Zero,
+        50.milliseconds,
+        planner,
+        Count
+      )
+      // system.terminate()
+  }
+}
+
